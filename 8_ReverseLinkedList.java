8f5c41cac37da96fbe9c623a99c57eba97206fa7
khushi bharti <khushibharti845@gmail.com>
2026-04-25 14:03:11 +0530

    [khushi] add:added ReverseLinkedList 25/4/26

---
+++ 8_ReverseLinkedList.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next; // store next
+            current.next = prev;             // reverse link
+            prev = current;                  // move prev
+            current = nextNode;             // move current
+        }
+
+        return prev;
+    }
+
+    // Function to print linked list
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        // Create linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original Linked List:");
+        printList(head);
+
+        Solution obj = new Solution();
+        ListNode reversedHead = obj.reverseList(head);
+
+        System.out.println("Reversed Linked List:");
+        printList(reversedHead);
+    }
+}
